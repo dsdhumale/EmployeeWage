943a6086c3fd888a34aa3419b94b84b9b9581dba
dsdhumale <d.sdhumale@gmail.com>
2022-10-03 17:35:43 +0530

    Added : Welcome Message

---
+++ EmployeeWages.java
@@ -0,0 +1,10 @@
+package com.Bridgelabz.EmpWage;
+
+public class EmployeeWages {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Welcome Every, Calculate Wages here");
+	}
+
+}
